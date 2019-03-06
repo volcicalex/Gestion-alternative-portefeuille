3bb92abe555b93a627f36c56088a49492e70d37a
lecalvmy <42962272+lecalvmy@users.noreply.github.com>
2019-03-06 11:12:21 +0100

    compo

--- Script_R/compositionPf.R
+++ Script_R/compositionPf.R
@@ -1,9 +1,10 @@
 # On recupere les donnees
 library(readxl)
+
 actifs <- data.frame(read_excel("../Excel/Selected_Actif_85-05.xlsx"))
 stock_numbers <- unique(actifs$stock_number)
 
-#Rentabilit? sur la p?riode en fonction des rentabilit?s mensuelles
+#Rentabilite sur la periode en fonction des rentabilites mensuelles
 calcul_rentab <- function(liste){
   r = 1
   l = length(liste)
@@ -14,7 +15,7 @@ calcul_rentab <- function(liste){
 }
 
 
-#On calcule la rentabilit? de chaque actif sur une ann?e donn?e pendant une p?riode
+#On calcule la rentabilite de chaque actif sur une annee donnee pendant une periode
 rentab <- function(annee_periode, mois_periode, taille_periode){
   dico_rentab=data.frame(actif=stock_numbers, rentab = rep(0,100))
   for (i in c(1:100)){
@@ -27,9 +28,9 @@ rentab <- function(annee_periode, mois_periode, taille_periode){
   return(dico_rentab)
 }
 
-#Cette fonction permet de calculer la composition des portefeuilles P1 ? P10 
-#On lui donne en entr?e la date de composition de notre portefeuille
-#Et la p?riode de pr?vision pour le portefeuille
+#Cette fonction permet de calculer la composition des portefeuilles P1 a P10 
+#On lui donne en entree la date de composition de notre portefeuille
+#Et la periode de prevision pour le portefeuille
 
 composition_portefeuille <- function(annee_periode, debut_periode, taille_periode){
   rentabilites = rentab(annee_periode - 1, debut_periode - taille_periode , taille_periode)
@@ -81,7 +82,7 @@ rentab_portefeuille(7,6)
 # Creation du fichier associant chaque actif a un portefeuille en tout temps
 # duree_etude correspond a la duree de l'etude en nombre d'annees (nombre entier)
 # duree_prec est le nombre de mois precedents a prendre en compte pour constituer le pf
-porteuille_annuel <- function(duree_etude, duree_prec){
+portefeuille_annuel <- function(duree_etude, duree_prec){
   constitution_annuelle <- data.frame(matrix(NA,ncol=100,nrow=20))
   rownames(constitution_annuelle) <- c(1985:2004)
   colnames(constitution_annuelle) <-  stock_numbers
@@ -96,3 +97,6 @@ rentab_actif <- rentab(1985, 1, 6)
 print(rentab_actif)
 portefeuilles <- composition_portefeuille(1986, 7, 6)
 print(portefeuilles)
+test <- portefeuille_annuel(20, 6)
+print(test)
+
